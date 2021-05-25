434cb40107bee6ebc40629662c2059f3c3a738a2
dream <2411507028@outlook.com>
2021-05-25 20:59:25 +0800

    miaomiaomiao

---
+++ dreambotcore/manage.py
@@ -0,0 +1,17 @@
+##############################
+# DreamBot Project 
+# Design by Dreamily854 
+##############################
+
+###################################
+# DreamBot Manager  API 
+###################################
+
+# E.G 
+# {
+#   "status":(True/false),
+#   "data":{...}
+# }
+
+
+# 1.LOGIN (NEED: username,password  RETURN: access token #
